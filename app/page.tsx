c05cb146ae256627f329eec0b57a68eba180b48c
Muhammad Bilal Khan <bkhan8423@gmail.com>
2025-03-26 20:58:55 +0500

    build fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import FAQ from "@/components/faq";
-import Features from "@/components/features";
-import Footer from "@/components/footer";
+// import FAQ from "@/components/faq";
+// import Features from "@/components/features";
+// import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import { Navbar } from "@/components/navbar";
-import Pricing from "@/components/pricing";
-import Testimonial from "@/components/testimonial";
+// import Pricing from "@/components/pricing";
+// import Testimonial from "@/components/testimonial";
 
 export default function Home() {
   return (
